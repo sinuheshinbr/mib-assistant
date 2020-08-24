04336def4586b0b3221c807faa74aeba632a0303
sinuheshinbr <66136384+sinuheshinbr@users.noreply.github.com>
2020-08-24 14:54:01 -0300

    x y inverted fixed

--- src/coordinates.js
+++ src/coordinates.js
@@ -9,41 +9,41 @@ const toCoordinates = (obj) => {
   let y;
   let mib = obj.ancient ? "AMIB" : "MIB";
   if (obj.latDirection.toLowerCase() === "n") {
-    dX1 = Number(obj.latMinute) / 60 + Number(obj.latDegree);
-    dX2 = (dX1 * 4096) / 360;
-    dX3 = 1624 - dX2;
-    if (dX3 < 0) {
-      x = Math.round(4096 + dX3);
+    dY1 = Number(obj.latMinute) / 60 + Number(obj.latDegree);
+    dY2 = (dY1 * 4096) / 360;
+    dY3 = 1624 - dY2;
+    if (dY3 < 0) {
+      y = Math.round(4096 + dY3);
     } else {
-      x = Math.round(dX3);
+      y = Math.round(dY3);
     }
   } else {
-    dX1 = Number(obj.latMinute) / 60 + Number(obj.latDegree);
-    dX2 = (dX1 * 4096) / 360;
-    dX3 = 1624 + dX2;
-    if (dX3 < 0) {
-      x = Math.round(4096 + dX3);
+    dY1 = Number(obj.latMinute) / 60 + Number(obj.latDegree);
+    dY2 = (dY1 * 4096) / 360;
+    dY3 = 1624 + dY2;
+    if (dY3 < 0) {
+      y = Math.round(4096 + dY3);
     } else {
-      x = Math.round(dX3);
+      y = Math.round(dY3);
     }
   }
   if (obj.lngDirection.toLowerCase() === "w") {
-    dY1 = Number(obj.lngMinute) / 60 + Number(obj.lngDegree);
-    dY2 = (dY1 * 5120) / 360;
-    dY3 = 1323 - dY2;
-    if (dY3 < 0) {
-      y = Math.round(5120 + dY3);
+    dX1 = Number(obj.lngMinute) / 60 + Number(obj.lngDegree);
+    dX2 = (dX1 * 5120) / 360;
+    dX3 = 1323 - dX2;
+    if (dX3 < 0) {
+      x = Math.round(5120 + dX3);
     } else {
-      y = Math.round(dY3);
+      x = Math.round(dX3);
     }
   } else {
-    dY1 = Number(obj.lngMinute) / 60 + Number(obj.lngDegree);
-    dY2 = (dY1 * 5120) / 360;
-    dY3 = 1323 + dY2;
-    if (dY3 < 0) {
-      y = Math.round(5120 + dY3);
+    dX1 = Number(obj.lngMinute) / 60 + Number(obj.lngDegree);
+    dX2 = (dX1 * 5120) / 360;
+    dX3 = 1323 + dX2;
+    if (dX3 < 0) {
+      x = Math.round(5120 + dX3);
     } else {
-      y = Math.round(dY3);
+      x = Math.round(dX3);
     }
   }
   return `+${mib}: ${x} ${y} 0 x`;
